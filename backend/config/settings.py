9e4a062c41ab8ad181e97fd7e95db25a1f1ffef1
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-09-12 12:48:12 +0430

    add apps to the installed_apps

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'quiz.apps.QuizConfig',
+    'question.apps.QuestionConfig',
+    'answers.apps.AnswersConfig',
 ]
 
 MIDDLEWARE = [
