32f97460fcb535a58e3f2ee3a6d8e5dc9afc94e4
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-09-12 12:48:35 +0430

    update question app

--- backend/question/admin.py
+++ backend/question/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Question
+from answers.models import Answer
 # Register your models here.
+
+
+class AnswerInline(admin.TabularInline):
+    model=Answer
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines=[AnswerInline]
+
+admin.site.register(Question,QuestionAdmin)
--- backend/question/models.py
+++ backend/question/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from answers.models import Answer
 # Create your models here.
+class Question(models.Model):
+    title=models.CharField(max_length=500)
+    answers=models.ManyToManyField(Answer,related_name="question")
