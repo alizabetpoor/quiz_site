8ff4103ec66c34da117cf10339302df31b9b36b0
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-09-12 20:27:20 +0430

    add result viewsets

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,15 @@
-from .serializers import QuizSerializer,QuestionSerializer,AnswerSerializer
+from .serializers import (
+    QuizSerializer,
+    QuestionSerializer,
+    AnswerSerializer,
+    ResultSerializer
+)
 from quiz.models import Quiz
 from rest_framework.response import Response
 from rest_framework import status
 from question.models import Question
 from answers.models import Answer
+from result.models import Result
 from rest_framework.generics import ListAPIView
 from rest_framework import viewsets
 # Create your views here.
@@ -40,3 +46,7 @@ class AnswersView(ListAPIView):
             return Response("این سوال وجود ندارد",status=status.HTTP_404_NOT_FOUND)
         serializer = AnswerSerializer(queryset,many=True)
         return Response(serializer.data)
+
+class ResultViewsets(viewsets.ModelViewSet):
+    queryset=Result.objects.all()
+    serializer_class=ResultSerializer
