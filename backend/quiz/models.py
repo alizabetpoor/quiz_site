3b7be77ee93ab05d200fadfdedb4de1f229963e1
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-09-12 11:50:07 +0430

    complete the model of the quiz

--- backend/quiz/models.py
+++ backend/quiz/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+
+class Quiz(models.Model):
+    name=models.CharField(max_length=120,verbose_name="نام کوییز")
+    time_quiz=models.IntegerField(validators=[MaxValueValidator(180,"زمان آزمون باید کمتر از 3 ساعت باشد"),
+        MinValueValidator(5,"زمان آزمون باید بیشتر از 5 دقیقه باشد")],
+        verbose_name="تایم آزمون(دقیقه)")
+    description=models.CharField(max_length="300",verbose_name="توضیح آزمون")
+    questions=models.ManyToManyField(null=True,related_name="quiz")
