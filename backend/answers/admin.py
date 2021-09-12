b1b52d602edeade914a8dd9b24cb292ddcf14793
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-09-12 19:52:11 +0430

    register answer model to the django admin

--- backend/answers/admin.py
+++ backend/answers/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Answer
 # Register your models here.
 
-admin.site.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display=["title","question","correct"]
+
+admin.site.register(Answer,AnswerAdmin)
