a43739149241dc0f216fa43bd86b345f7dd73314
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-09-13 15:25:29 +0430

    add quizpage

---
+++ frontend/src/pages/QuizPage.js
@@ -0,0 +1,10 @@
+import Questions from "../Components/Questions/Questions";
+const QuizPage = (props) => {
+  return (
+    <>
+      <Questions {...props} />
+    </>
+  );
+};
+
+export default QuizPage;
