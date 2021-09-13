de42f4f6a0ccf50ba57dcb6ef7d5808b355ce951
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-09-13 11:55:23 +0430

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
-
+import { Route, Switch } from "react-router-dom";
+import Routes from "./Routes";
+import Layout from "./layout/Layout";
 function App() {
   return (
     <div className="App">
-      <h2>start</h2>
+      <Layout>
+        <div className="main w-full flex justify-center">
+          <Switch>
+            {Routes.map((route, index) => {
+              return <Route key={index} {...route} />;
+            })}
+          </Switch>
+        </div>
+      </Layout>
     </div>
   );
 }
