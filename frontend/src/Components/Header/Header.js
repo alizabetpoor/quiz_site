549d0228fdaad0278024fe77118bdb037362f512
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-09-13 11:53:37 +0430

    update header

---
+++ frontend/src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div
+      style={{ backgroundColor: "#3B185F" }}
+      className="header h-14 flex justify-center items-center text-white"
+    >
+      <p>به سایت آزمون خوش آمدید</p>
+    </div>
+  );
+};
+
+export default Header;
