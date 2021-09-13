85dc9331cf08cc6d3f1a40320740515df964130b
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-09-13 11:47:49 +0430

    add cors header to the project

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     ##3rd party
     'rest_framework',
+    "corsheaders",
 
     ## local
     'quiz.apps.QuizConfig',
@@ -52,6 +53,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,3 +135,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS=[
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
