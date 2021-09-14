27565d5aa9781f0898faa4fb7071e90f6a71fbbf
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-09-14 12:11:20 +0430

    add user detail to the header

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -1,10 +1,45 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getUserMeApi } from "../../Services/UserServiceMethods";
 const Header = () => {
+  const [token, setToken] = useState("");
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setToken(token);
+    }
+  }, []);
+  useEffect(() => {
+    if (token !== "") {
+      const tok = `Token ${token}`;
+      getUserMeApi(tok).then((res) => setUser(res.data));
+    }
+  }, [token]);
   return (
     <div
       style={{ backgroundColor: "#3B185F" }}
-      className="header h-14 flex justify-center items-center text-white"
+      className="header h-14 flex justify-around items-center text-white"
     >
-      <p>به سایت آزمون خوش آمدید</p>
+      <div>
+        <Link to="/" className="px-4 py-1 bg-blue-400 rounded-lg">
+          صفحه اصلی
+        </Link>
+      </div>
+      <div>
+        <p>به سایت آزمون خوش آمدید</p>
+      </div>
+      <div>
+        {token ? (
+          <p className="px-4 py-1 bg-green-400 font-normal rounded-lg text-lg text-black">
+            {user.username}
+          </p>
+        ) : (
+          <Link to="/login" className="px-4 py-1 bg-blue-400 rounded-lg">
+            ورود
+          </Link>
+        )}
+      </div>
     </div>
   );
 };
