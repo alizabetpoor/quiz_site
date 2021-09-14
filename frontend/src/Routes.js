9ea48636e4275eba431162f7709d2fb7d30645dc
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-09-14 12:12:33 +0430

    update routes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,8 +1,12 @@
 import HomePage from "./pages/HomePage";
 import QuizPage from "./pages/QuizPage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
 const Routes = [
   { path: "/", exact: true, component: HomePage },
   { path: "/quiz/:quizid", component: QuizPage },
+  { path: "/login", component: LoginPage },
+  { path: "", component: NotFoundPage },
 ];
 
 export default Routes;
