a9e466f36ef5dc5e2477abd9ced5eae82a005d55
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-09-14 14:12:52 +0430

    add result route

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,10 +2,12 @@ import HomePage from "./pages/HomePage";
 import QuizPage from "./pages/QuizPage";
 import LoginPage from "./pages/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import ResultPage from "./pages/ResultPage";
 const Routes = [
   { path: "/", exact: true, component: HomePage },
   { path: "/quiz/:quizid", component: QuizPage },
   { path: "/login", component: LoginPage },
+  { path: "/result", component: ResultPage },
   { path: "", component: NotFoundPage },
 ];
 
