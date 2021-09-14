59e5e908c198711f6cadd53a26c768fea6f3c32e
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-09-14 14:11:25 +0430

    add logout api

--- frontend/src/Services/UserServiceMethods.js
+++ frontend/src/Services/UserServiceMethods.js
@@ -3,7 +3,18 @@ import http from "./BaseService";
 export const loginApi = (data) => {
   return http.post("/auth/token/login", data);
 };
+export const logoutApi = (token) => {
+  return http.post(
+    "/auth/token/logout",
+    {},
+    {
+      headers: { authorization: "token " + token },
+    }
+  );
+};
 
 export const getUserMeApi = (token) => {
-  return http.get("/auth/users/me", { headers: { authorization: token } });
+  return http.get("/auth/users/me", {
+    headers: { authorization: "token " + token },
+  });
 };
