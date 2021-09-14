e53253a18e96a4accad7d90c71cc8ff9c29f93ff
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-09-14 12:09:42 +0430

    add notfound page

---
+++ frontend/src/pages/NotFoundPage.js
@@ -0,0 +1,13 @@
+import Layout from "../layout/Layout";
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <div className="notfound">
+        <h2>صفحه مورد نظر پیدا نشد</h2>
+        <h4>404</h4>
+      </div>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
