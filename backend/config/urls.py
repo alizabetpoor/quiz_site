0ea7474348ad5830c469c08d1f29a3bcde003437
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-09-14 09:48:13 +0430

    add drf_yasg setting

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,9 +16,32 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include,re_path
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Quiz API",
+      default_version='v1',
+      description="A simple quiz app",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="ali_0_zabetpoor@ymail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include("api.urls")),
     path('api/v1/auth/', include('djoser.urls')),
     path('api/v1/auth/', include('djoser.urls.authtoken')),
+    re_path(r'api/v1/swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'api/v1/swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'api/v1/redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
