af5e7b8b11f5b8f785cf80c96e0920a0cc1a2275
alizabetpoor <ali_0_zabetpoor@ymail.com>
2021-09-14 09:48:33 +0430

    update

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -143,7 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #REST FRAMEWORK SETTING
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':[
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ],
 }
